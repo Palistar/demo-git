1a50150d2df2b3374428665c3a33a926ee3a41b1
Charly Paradis <charlyparadis@hotmail.com>
2024-11-06 19:51:55 -0500

    Hello

---
+++ js/app.js
@@ -0,0 +1,7 @@
+"using strict";
+
+document.getElementById("haha").addEventListener("mouseover", onMouseOver, false);
+
+function onMouseOver() {
+    document.getElementById("haha").textContent = "Not Anymore! >:3";
+}
